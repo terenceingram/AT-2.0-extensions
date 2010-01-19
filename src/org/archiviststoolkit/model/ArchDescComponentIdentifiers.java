84e81d65fa31d8c4cdb45ce99f55c403dd0ac9d6
Terence Ingram <tingram@pc200236.nla.gov.au>
2010-01-20 09:30:20 +1100

    Added ArchDescComponentIdentifiers to the model. This meant changes to other classes in the model package that have a relationship with the new TABLE. Also amended all the hibernate config files that are affected.
     Committer: Terence Ingram <tingram@pc200236.nla.gov.au>
    
     On branch master
     Changes to be committed:
    	new file:   org/archiviststoolkit/model/ArchDescComponentIdentifiers.hbm.xml
    	new file:   org/archiviststoolkit/model/ArchDescComponentIdentifiers.java
    	modified:   hibernate.cfg.xml
    	modified:   org/archiviststoolkit/model/Resources.hbm.xml
    	modified:   org/archiviststoolkit/model/Resources.java
    	modified:   org/archiviststoolkit/model/ResourcesComponents.hbm.xml
    	modified:   org/archiviststoolkit/model/ResourcesComponents.java

---
+++ src/org/archiviststoolkit/model/ArchDescComponentIdentifiers.java
@@ -0,0 +1,108 @@
+package org.archiviststoolkit.model;
+
+import org.archiviststoolkit.mydomain.DomainObject;
+import org.archiviststoolkit.structure.ExcludeFromDefaultValues;
+import org.archiviststoolkit.structure.IncludeInApplicationConfiguration;
+
+
+/**
+ * 
+ * @author tingram
+ *
+ */
+public class ArchDescComponentIdentifiers extends DomainObject implements Comparable {
+	
+	private static final long serialVersionUID = -2014304772098808331L;
+	
+	public static final String PROPERTYNAME_COMPONENT_IDENTIFIER = "componentIdentifier";
+	public static final String PROPERTYNAME_IDENTIFIER_TYPE = "identifierType";
+	public static final String PROPERTYNAME_IDENTIFIER_LABEL = "identifierLabel";
+	
+	//@IncludeInApplicationConfiguration
+	@ExcludeFromDefaultValues
+	private Long archDescComponentId;
+	
+	@ExcludeFromDefaultValues
+	private String componentIdentifier;
+	
+	@ExcludeFromDefaultValues
+	private String identifierType;
+	
+	@ExcludeFromDefaultValues
+	private String identifierLabel;
+	
+	private ResourcesComponents resourceComponent;
+	private Resources resource;
+	
+	public ArchDescComponentIdentifiers() {
+	}
+	
+	public ArchDescComponentIdentifiers(Resources resource) {
+		this.resource = resource;
+	}
+	
+	
+	@Override
+	public Long getIdentifier() {
+		return getArchDescComponentId();
+	}
+
+	@Override
+	public void setIdentifier(Long identifier) {
+		setArchDescComponentId(identifier);
+	}
+	
+	
+	// Getters and Setters
+	
+	public Long getArchDescComponentId() {
+		return archDescComponentId;
+	}
+
+	public void setArchDescComponentId(Long archDescComponentId) {
+		this.archDescComponentId = archDescComponentId;
+	}
+
+	public String getComponentIdentifier() {
+		return componentIdentifier;
+	}
+
+	public void setComponentIdentifier(String componentIdentifier) {
+		this.componentIdentifier = componentIdentifier;
+	}
+
+	public String getIdentifierType() {
+		return identifierType;
+	}
+
+	public void setIdentifierType(String identifierType) {
+		this.identifierType = identifierType;
+	}
+
+	public String getIdentifierLabel() {
+		return identifierLabel;
+	}
+
+	public void setIdentifierLabel(String identifierLabel) {
+		this.identifierLabel = identifierLabel;
+	}
+
+	public ResourcesComponents getResourceComponent() {
+		return resourceComponent;
+	}
+
+	public void setResourceComponent(ResourcesComponents resourceComponent) {
+		this.resourceComponent = resourceComponent;
+	}
+
+	public Resources getResource() {
+		return resource;
+	}
+
+	public void setResource(Resources resource) {
+		this.resource = resource;
+	}
+	
+	
+
+}
